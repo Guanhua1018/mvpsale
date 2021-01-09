80664d24c670c20e84d529be6f001010e1d99c28
Chris <guanhuali1018@outlook.com>
2021-01-09 14:37:15 -0500

    fixed search bar in small screen

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Row, Col } from 'react-bootstrap';
 
 const SearchBox = ({ history }) => {
   const [keyword, setKeyword] = useState('');
@@ -15,16 +15,22 @@ const SearchBox = ({ history }) => {
 
   return (
     <Form onSubmit={submitHandler} inline>
-      <Form.Control
-        type='text'
-        name='q'
-        onChange={(e) => setKeyword(e.target.value)}
-        placeholder='Search Products...'
-        className='mr-sm-2 ml-sm-5'
-      ></Form.Control>
-      <Button type='submit' variant='outline-success' className='p-2'>
-        Search
-      </Button>
+      <Row>
+        <Col xs='auto'>
+          <Form.Control
+            type='text'
+            name='q'
+            onChange={(e) => setKeyword(e.target.value)}
+            placeholder='Search...'
+            className='mr-sm-2 ml-sm-5'
+          ></Form.Control>
+        </Col>
+        <Col xs='mr-xs-0'>
+          <Button type='submit' variant='outline-success'>
+            Search
+          </Button>
+        </Col>
+      </Row>
     </Form>
   );
 };
