9e4be2b907e6fac7d58077e24835a08204e51c6f
ChrisLi <guanhuali1018@outlook.com>
2021-02-02 15:00:35 -0500

    fixed

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -79,7 +79,7 @@ const CartScreen = ({ match, location, history }) => {
                       variant='light'
                       onClick={() => removeFormCartHandler(item.product)}
                     >
-                      <i className='fas fa-trash'></i>
+                      <i className='fas fa-trash'/>
                     </Button>
                   </Col>
                 </Row>
